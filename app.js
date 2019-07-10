a1f72726bb1010186502843501bd1429901ed158
Brian Dorry <brian.dorry@vonage.com>
2019-07-10 11:44:19 -0400

    improved error handling

--- app.js
+++ app.js
@@ -28,40 +28,19 @@ app.get('/', (req, res) => {
 })
 
 app.post('/event', (req, res) => {
-  res.json({ok:true}); // status 200 is default
+  res.json({ok: true}); // status 200 is default
 })
 
 app.get('/answer', (req, res) => {
   var time = new Date();
- 
-  getCurrentEvent(time, (currentEvent) => {
-  	var ncco;
-		if(currentEvent){
-	    var startTime = moment(currentEvent.start.dateTime);
-	    var endTime = moment(currentEvent.end.dateTime);
 
-	    ncco = [
-		    {
-		      action: "talk",
-		      voiceName: voiceName,
-		      text: "Welcome to the business hours demo app!"
-		    },
-		    {
-		      action: "talk",
-		      voiceName: voiceName,
-		      text: "Our business hours today are from " + startTime.utcOffset(utcOffset).format("LT") + " to " + endTime.utcOffset(utcOffset).format("LT")
-		    }
-		  ]
-	  } else {
-	    ncco = [{
-	      action: "talk",
-	      voiceName: voiceName,
-	      text: "We are current closed."
-	    }]
-	  }
+  getCurrentEvent(time).then((result) => {
+    res.json(buildResponse(time, result));      
+  }).catch(error => {
+    console.log(error)
+    res.json(buildErrorResponse(error));
+  })
 
-	  res.json(ncco);
-  });
 })
 
 function getCurrentEvent(time, callback){
@@ -72,23 +51,63 @@ function getCurrentEvent(time, callback){
   timeMax.setDate(timeMax.getDate() + 1);
   timeMax.setHours(0,0,0);
 
-  cal.events.list({
+  return cal.events.list({
     timeMin: new Date(timeMin).toISOString(),
     timeMax: new Date(timeMax).toISOString(),
     singleEvents: true,
     calendarId: calendarUri
-  }, (error, result) => {
-    for (let item of result.data.items) {
-      var startTime = moment(item.start.dateTime);
-      var endTime = moment(item.end.dateTime);
-
-      if(startTime.toDate() <= time && endTime.toDate() > time){
-      	callback(item)
-      	break;
-      }
-    } 
-    callback(null)
   })
+}
+
+function buildResponse(time, result){
+  var event = parseEventResponse(time, result);
+  var ncco = []
+
+  if(event){
+    var startTime = moment(event.start.dateTime);
+    var endTime = moment(event.end.dateTime);
+
+    ncco.push({
+      action: "talk",
+      voiceName: voiceName,
+      text: "Welcome to the business hours demo app!"
+    })
+    ncco.push({
+      action: "talk",
+      voiceName: voiceName,
+      text: "Our business hours today are from " + startTime.utcOffset(utcOffset).format("LT") + " to " + endTime.utcOffset(utcOffset).format("LT")
+    })
+  } else {
+    ncco.push({
+      action: "talk",
+      voiceName: voiceName,
+      text: "We are current closed."
+    })
+  }
+
+  return ncco;
+}
+
+function parseEventResponse(time, result){
+  for (let item of result.data.items) {
+    var startTime = moment(item.start.dateTime);
+    var endTime = moment(item.end.dateTime);
+
+    if(startTime.toDate() <= time && endTime.toDate() > time){
+      console.log("Found current event: " + item.id)
+      return item;
+    }
+  } 
+  console.log("Unable to find current event")
+  return null;
+}
+
+function buildErrorResponse(error){
+  return [{
+    action: "talk",
+    voiceName: voiceName,
+    text: "An error occurred connecting to your calendar"
+  }]
 }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
